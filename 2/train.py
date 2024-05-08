c348eb14699644c04f6a8910e27033bddc4cd204
CAMB <cambdev@outlook.com>
2024-05-08 16:58:21 +0800

    feat: doc2vec

--- 2/train.py
+++ 2/train.py
@@ -1,36 +1,63 @@
 import os
 import re
+import smart_open
+import gensim
 from gensim.models import word2vec
 from gensim.models import doc2vec
 import jieba
+import logging
 
-chs_match_pattern = re.compile(r'[0-9a-zA-Z\u4e00-\u9fa5]+')
+logging.basicConfig(
+    format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO
+)
+
+chs_match_pattern = re.compile(r"[0-9a-zA-Z\u4e00-\u9fa5]+")
 
 
 def clean_text(text: str) -> str:
-    return ''.join(chs_match_pattern.findall(text))
+    return "".join(chs_match_pattern.findall(text))
 
 
 def train_data_list() -> list[str]:
     r = []
-    for root, dirs, files in os.walk('2019'):
+    for root, dirs, files in os.walk("2019"):
         for file in files:
             r.append(os.path.join(root, file))
     return r
 
 
 def train_data_read(filepath: str) -> str:
-    f = open(filepath, 'r', encoding='utf-8')
-    data = clean_text(''.join(f.readlines()))
+    f = open(filepath, "r", encoding="utf-8")
+    data = clean_text("".join(f.readlines()))
     f.close()
     return data
 
 
-if __name__ == '__main__':
+def read_corpus(texts: list[str]) -> list[doc2vec.TaggedDocument]:
+    r = []
+    for i, text in enumerate(texts):
+        tokens = gensim.utils.simple_preprocess(text)
+        r.append(doc2vec.TaggedDocument(tokens, [i]))
+    return r
+
+
+if __name__ == "__main__":
     data_filepath_list = train_data_list()
     datas: list[str] = []
     for i in data_filepath_list:
         datas.append(train_data_read(i))
-    sens_list = [jieba.lcut(i) for i in data_filepath_list]
-    model = word2vec.Word2Vec(sens_list, min_count=1, epochs=20)
+    print(len(datas))
+
+    sens_list = [jieba.lcut(i) for i in datas]
+    model = word2vec.Word2Vec(sens_list, min_count=5, epochs=1000)
+    model.build_vocab(corpus_iterable=sens_list)
+    model.train(
+        corpus_iterable=datas, total_examples=model.corpus_count, epochs=model.epochs
+    )
     model.save("word2vec.model")
+
+    train_corpus = list(read_corpus(datas))
+    model = doc2vec.Doc2Vec(vector_size=50, min_count=5, epochs=1000)
+    model.build_vocab(train_corpus)
+    model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
+    model.save("doc2vec.model")
